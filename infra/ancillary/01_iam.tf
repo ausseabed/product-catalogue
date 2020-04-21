63a742ea2675e9aa3eb1df67a1b1246f7c5b813e
DaveP <david.penton@ga.gov.au>
2020-04-21 03:48:29 +0000

    NGA-283 fixing orphaned tf resource reference

--- infra/ancillary/01_iam.tf
+++ infra/ancillary/01_iam.tf
@@ -1,3 +1,3 @@
-data "aws_iam_role" "aws_ecs_task_execution_role_svc" {
-  name = "aws_ecs_task_execution_role_svc"
+data "aws_iam_role" "ecs_task_execution_role_svc" {
+  name = "ecs_task_execution_role_svc"
 }
