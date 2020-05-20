0af2f37d1353811dc9d37032ea8d461db695358c
DaveP <david.penton@ga.gov.au>
2020-05-20 09:16:40 +0000

    NGA-327 Change name of rest folder

--- create.sh
+++ create.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
-mkdir asb-pc-api-fetch
-(cd asb-pc-api-fetch
+mkdir rest-client
+(cd rest-client
 curl http://localhost:3000/api-json | python -m json.tool > openapi-definition.json
 openapi-generator-typescript generate -g typescript -i openapi-definition.json \
---additional-properties="npmName=@ausseabed/pc-api-fetch,npmRepository=git://github.com/ausseabed/product-catalogue.git" 
+--additional-properties="npmName=@ausseabed/product-catalogue-rest-client,npmRepository=git://github.com/ausseabed/product-catalogue.git" 
 )
